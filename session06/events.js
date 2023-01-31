741c9868ccbb6b6e9708c239103b21348da06828
Tim <github@mailtim.de>
2023-01-31 15:58:58 +0100

    First iteration of ToDo List task done

--- session06/events.js
+++ session06/events.js
@@ -2,76 +2,93 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
     const todoListContainer = document.getElementById('todo-list');
     const textInput = document.getElementById('todo-text-input');
-
-    // Inline Variant without array
-
-    // form.addEventListener('submit', (e) => {
-    //     e.preventDefault();
-
-    //     const textInput = document.getElementById('todo-text-input');
-
-    //     const listItem = document.createElement('li');
-    //     listItem.innerText = textInput.value;
-
-    //     const deleteButton = document.createElement('button');
-    //     deleteButton.setAttribute('class', 'delete__button');
-    //     deleteButton.innerText = "🗑";
-
-    //     deleteButton.addEventListener('click', clickEvent => {
-    //         clickEvent.target.parentNode.remove();
-    //     });
-
-    //     listItem.appendChild(deleteButton);
-
-    //     todoListContainer.appendChild(listItem);
-
-    //     textInput.value = '';
-    // });
+    const dateInput = document.getElementById('todo-date-input');
+    const prioInput = document.getElementById('todo-prio-input');
+    let sortState = 0;
+    const sortBtn = document.getElementById('sort-btn');
+    const sortIndicator = document.getElementById('sort-indicator');
 
     let todoItems = [];
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        addItem(todoListContainer, todoItems, textInput.value);
+        addItem(todoListContainer, todoItems, textInput.value, dateInput.value, prioInput.value);
         textInput.value = '';
+    });
+
+    sortBtn.addEventListener('click', () => {
+
+        sortItems(todoListContainer, todoItems, 'prio', sortState, sortIndicator);
+        sortState = ++sortState % 2;
     });
 });
 
 function generateID(container, todoItems, item) {
-    return `${container.getAttribute('id')}-todoitem-${item}-${todoItems[item].replaceAll(' ', '-').replaceAll('"', '').replaceAll("'", '')}`;
+    return `${container.getAttribute('id')}-todoitem-${item}-${todoItems[item].text
+        .replaceAll(' ', '-')
+        .replaceAll('"', '')
+        .replaceAll("'", '')}`;
+}
+
+function sortItems(container, todoItems, key, sortState, sortIndicator) {
+    const sortingFunctions = [
+        (a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0),
+        (a, b) => (a[key] < b[key] ? 1 : a[key] > b[key] ? -1 : 0),
+    ];
+
+    const arrows = ['↑', '↓'];
+    sortIndicator.innerText = arrows[sortState];
+    
+    todoItems.sort(sortingFunctions[sortState]);
+
+    displayItems(container, todoItems);
 }
 
 function displayItems(container, todoItems) {
     container.querySelectorAll('.todoitem').forEach((i) => i.remove());
 
+    const priorities = ['low', 'normal', 'high'];
+
     for (let index in todoItems) {
+        const prioText = priorities[todoItems[index].prio];
 
         const listItem = document.createElement('li');
-        listItem.innerText = todoItems[index];
-        listItem.setAttribute('class', 'todoitem');
+        listItem.setAttribute('class', `todoitem todoitem--prio-${prioText}`);
         listItem.setAttribute('id', generateID(container, todoItems, index));
 
-        const deleteButton = document.createElement('button');
-        deleteButton.setAttribute('class', 'delete__button');
-        deleteButton.innerText = '🗑';
-
+        const deleteButton = createItemComponent('span', 'todoitem__delete-btn', '✓');
         deleteButton.addEventListener('click', () => deleteItem(container, todoItems, index));
 
         listItem.appendChild(deleteButton);
+
+        const title = createItemComponent('span', 'todoitem__title', todoItems[index].text);
+        const dueDate = createItemComponent('span', 'todoitem__duedate', todoItems[index].date);
+        const prio = createItemComponent('span', 'todoitem__prio', prioText);
+        listItem.appendChild(title);
+        listItem.appendChild(dueDate);
+        listItem.appendChild(prio);
 
         container.appendChild(listItem);
     }
 }
 
-function addItem(container, todoItems, item) {
-    todoItems.push(item);
+function createItemComponent(type, classes, content) {
+    const component = document.createElement(type);
+    component.setAttribute('class', classes);
+    component.innerText = content;
+    return component;
+}
+
+function addItem(container, todoItems, itemText, itemDueDate, itemPrio) {
+    todoItems.push({
+        text: itemText,
+        date: itemDueDate,
+        prio: itemPrio,
+    });
     displayItems(container, todoItems);
 }
 
 function deleteItem(container, todoItems, index) {
-    // todoItems.splice(index, 1);
-    // displayItems(container, todoItems);
-
     let elem = document.getElementById(generateID(container, todoItems, index));
     elem.style.opacity = 0;
     elem.style.setProperty('transform', 'translateX(-25px)');
